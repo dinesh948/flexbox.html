0beb497fec4133bfd37c88f8c92799ed2cb1d9b5
dinesh948 <elisettydineshkumar27@gmail.com>
2026-02-27 10:15:35 +0530

    Script.js

---
+++ Script.js
@@ -0,0 +1,18 @@
+var a=10
+{
+    console.log(5+"5");
+    console.log(5-"5");
+    var a=20
+    let arr=[1,'new',2,3]
+    console.log(arr);
+
+}
+console.log(a);
+let mark=30;
+let result= mark>=40?'pass':'fail'
+console.log(result);
+console.log(18^8)
+ function subtract(a,b){
+    console.log(a-b)
+ }
+ subtract(3333,111)
